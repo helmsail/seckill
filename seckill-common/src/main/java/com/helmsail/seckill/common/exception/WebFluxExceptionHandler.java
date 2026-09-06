51a0de21c20f7543cee1b603c1a04392ab3a8192
hejye <1959365428@qq.com>
2026-09-06 17:43:28 +0800

    feat(common): 集成 Log4j2 高性能日志，支持 MDC traceId/userId 输出

--- seckill-common/src/main/java/com/helmsail/seckill/common/exception/WebFluxExceptionHandler.java
+++ seckill-common/src/main/java/com/helmsail/seckill/common/exception/WebFluxExceptionHandler.java
@@ -2,6 +2,7 @@ package com.helmsail.seckill.common.exception;
 
 import com.helmsail.seckill.common.result.Result;
 import com.helmsail.seckill.common.result.ResultEnum;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -14,6 +15,7 @@ import reactor.core.publisher.Mono;
  *
  * 仅在 Reactive 环境下生效。
  */
+@Slf4j
 @RestControllerAdvice
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -21,11 +23,13 @@ public class WebFluxExceptionHandler {
 
     @ExceptionHandler(BizException.class)
     public Mono<Result<?>> handleBizException(BizException e) {
+        log.warn("业务异常: code={}, message={}", e.getCode(), e.getMessage());
         return Mono.just(Result.of(e.getCode(), e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public Mono<Result<?>> handleException(Exception e) {
+        log.error("系统异常", e);
         return Mono.just(Result.of(ResultEnum.SYSTEM_ERROR));
     }
 }
--- seckill-common/src/main/java/com/helmsail/seckill/common/exception/WebMvcExceptionHandler.java
+++ seckill-common/src/main/java/com/helmsail/seckill/common/exception/WebMvcExceptionHandler.java
@@ -2,6 +2,7 @@ package com.helmsail.seckill.common.exception;
 
 import com.helmsail.seckill.common.result.Result;
 import com.helmsail.seckill.common.result.ResultEnum;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  *
  * 仅在 Servlet 环境下生效。
  */
+@Slf4j
 @RestControllerAdvice
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -20,11 +22,13 @@ public class WebMvcExceptionHandler {
 
     @ExceptionHandler(BizException.class)
     public Result<?> handleBizException(BizException e) {
+        log.warn("业务异常: code={}, message={}", e.getCode(), e.getMessage());
         return Result.of(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public Result<?> handleException(Exception e) {
+        log.error("系统异常", e);
         return Result.of(ResultEnum.SYSTEM_ERROR);
     }
 }
