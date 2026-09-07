a509c115e96a1616510a51f0c1e23841ad7101d0
hejye <1959365428@qq.com>
2026-09-07 20:43:11 +0800

    feat: 新增 seckill-processor 消息处理模块

---
+++ seckill-processor/src/main/java/com/helmsail/seckill/processor/SeckillProcessorApplication.java
@@ -0,0 +1,15 @@
+package com.helmsail.seckill.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 秒杀系统消息处理模块启动类
+ */
+@SpringBootApplication
+public class SeckillProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillProcessorApplication.class, args);
+    }
+}
