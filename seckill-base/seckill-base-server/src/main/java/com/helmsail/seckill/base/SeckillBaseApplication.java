a4611ac508eb4897440782722efa0322d235f18c
hejye <1959365428@qq.com>
2026-09-07 12:30:54 +0800

    feat: 新增 seckill-base 模块，搭建秒杀域基础骨架

---
+++ seckill-base/seckill-base-server/src/main/java/com/helmsail/seckill/base/SeckillBaseApplication.java
@@ -0,0 +1,15 @@
+package com.helmsail.seckill.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 秒杀域基础模块启动类
+ */
+@SpringBootApplication
+public class SeckillBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillBaseApplication.class, args);
+    }
+}
