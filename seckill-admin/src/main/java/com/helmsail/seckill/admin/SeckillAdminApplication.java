39ee58267ab2c6fbe3ce4eac5dea1d40e2c275f3
hejye <1959365428@qq.com>
2026-09-07 15:31:06 +0800

    feat: 新增 seckill-admin 编排层模块骨架

---
+++ seckill-admin/src/main/java/com/helmsail/seckill/admin/SeckillAdminApplication.java
@@ -0,0 +1,15 @@
+package com.helmsail.seckill.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 秒杀系统编排层启动类
+ */
+@SpringBootApplication
+public class SeckillAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillAdminApplication.class, args);
+    }
+}
