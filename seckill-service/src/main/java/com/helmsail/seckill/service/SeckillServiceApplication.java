c3d7d7b7d3207d98ddcecc598ad3511514265ca9
hejye <1959365428@qq.com>
2026-09-07 20:33:44 +0800

    feat: 新增 seckill-service 服务层模块，统一 POM 版本管理

---
+++ seckill-service/src/main/java/com/helmsail/seckill/service/SeckillServiceApplication.java
@@ -0,0 +1,15 @@
+package com.helmsail.seckill.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 秒杀系统服务层启动类
+ */
+@SpringBootApplication
+public class SeckillServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillServiceApplication.class, args);
+    }
+}
