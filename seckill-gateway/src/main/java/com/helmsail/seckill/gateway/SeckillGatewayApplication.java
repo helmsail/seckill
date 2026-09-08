7078e5ffb0129448134895893fd21f4ba08545dc
hejye <1959365428@qq.com>
2026-09-08 22:26:41 +0800

    feat: 新增 seckill-gateway 网关模块骨架

---
+++ seckill-gateway/src/main/java/com/helmsail/seckill/gateway/SeckillGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.helmsail.seckill.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillGatewayApplication.class, args);
+    }
+}
